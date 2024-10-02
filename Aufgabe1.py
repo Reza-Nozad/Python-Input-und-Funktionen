295a0993f6081c248548064f9fee921c62ee5648
reza <reza.nozad@gmail.com>
2024-10-02 13:39:11 +0200

    Die Aufgabe 1 ist erledigt!

---
+++ Aufgabe1.py
@@ -0,0 +1,11 @@
+
+zahl=int(input("geben Sie bitte einen Zahl ein:"))
+print("Ihre Zahl ist = ",zahl)
+if zahl == 0:
+	print("der Zahl ist 0")
+elif zahl > 0:
+	print("der Zahl ist positiv")
+elif zahl < 0:
+    print("der Zahl ist negativ")
+else:
+    print("geben Sie bitte einen Zahl ein")
