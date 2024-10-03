86b0f0cc4a2e462a2901379533353dc8446b9388
reza <reza.nozad@gmail.com>
2024-10-03 21:16:48 +0200

    die Aufgabe4 ist erledigt!

---
+++ Aufgabe4.py
@@ -0,0 +1,10 @@
+def sum_of_squares():
+ zahl1=int(input("geben Sie bitte ersten Zahl ein:  ")) 
+ print("Ihre erster Zahl ist: ", zahl1)   
+ zahl2=int(input("geben Sie bitte zweiten Zahl ein: "))
+ print("Ihre zweiter Zahl ist: ", zahl2)
+ sum=(zahl1**2)+(zahl2**2)
+ print("die Summe der Quadrate", zahl1, "und", zahl2, "ist =", sum)
+ 
+sum_of_squares()
+
